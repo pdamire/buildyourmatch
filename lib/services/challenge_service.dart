bb4230d4371223eb3ba28c4f5c9419a75b14df7f
Amirhossein Emami <amirhosseinemami@gmail.com>
2025-11-21 21:01:44 -0800

    Fix: replace invalid .not() with .notIn()

--- lib/services/challenge_service.dart
+++ lib/services/challenge_service.dart
@@ -97,7 +97,7 @@ class ChallengeService {
         .limit(20);
 
     if (answeredIds.isNotEmpty) {
-      query.not('id', 'in', answeredIds);
+      query.notIn('id', 'in', answeredIds);
     }
 
     final res = await query;
