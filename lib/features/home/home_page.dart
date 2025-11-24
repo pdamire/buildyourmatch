96552fae83b86c3c7b76596db5fda7d80a1cb5d4
Amirhossein Emami <amirhosseinemami@gmail.com>
2025-11-23 20:28:35 -0800

    Fix navigation braces and profile page syntax

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,6 +1,4 @@
-import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -214,8 +212,6 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
-  }
-}
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import file_selector_macos
 import path_provider_foundation
 import printing
 import purchases_flutter
@@ -15,6 +16,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
